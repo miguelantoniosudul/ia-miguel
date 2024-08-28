a2ce0432b5466e5e899f323ac415ed82e53ec78d
miguelantoniosudul <103214003+miguelantoniosudul@users.noreply.github.com>
2024-08-28 10:16:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, pode gerear empregos e economizar recursos.",
-                afirmacao: "A reciclagem pode gerar empregos e economizar recursos, contribuindo para o crescimento econômico e a preservação ambiental ao reduzir a necessidade de matérias-primas. "
+                afirmacao: [
+                    "A reciclagem pode gerar empregos e economizar recursos, contribuindo para o crescimento econômico e a preservação ambiental ao reduzir a necessidade de matérias-primas. ",
+                    "A reciclagem pode impulsionar a economia ao gerar novas vagas de trablho e reduzir os gastos com a produççao de novos materiais, além de diminuir a pressão sobre os recursos naturais."
+                ]
             },
             {
                 texto: "Não, acho que os custos superam os benefícios. ",
-                afirmacao: "A reciclagem pode não gerar tantos empregos quanto esperado e, em alguns casos, pode não ser eficiente na economia de recursos."
+                afirmacao: [
+                    "A reciclagem pode não gerar tantos empregos quanto esperado e, em alguns casos, pode não ser eficiente na economia de recursos.",
+                    "Embora existam custos iniciais, a reciclgem pode resultar em econômia a longo prazo ao reduzir a necessidade de extração de novos recurços e diminuir o impacto ambiental."
+
+                ]
             }
         ]
     },
@@ -23,11 +30,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sempre procuro produtos sutentáveis e de baixo impacto",
-                afirmacao: "Sempre procuro reduzir minha pegada ecológica e apoiar praticas sustentáveis."
+                afirmacao: [
+                    "Sempre procuro reduzir minha pegada ecológica e apoiar praticas sustentáveis.",
+                    "Procurar produtos sustentáveis e de baixo impacto garantem que suas compras minimizem os danos ao meio meio ambiente e contribuam para práticas mais responsáveis e ecológicas."
+
+                ]
             },
             {
                 texto: "Não considero isso na hora de comprar",
-                afirmacao: "a falta de conscientização sobre suas consequências ambientais."
+                afirmacao: [
+                    "a falta de conscientização sobre suas consequências ambientais.",
+                    "Não considerar o impacto ambiental dos ptodutos na hora da compra pode ser um erro pois isso contribui para degradação ambiental e o esgotamento dos rescursos naturais. "
+                ]
+            
             }
         ]
     },
@@ -36,11 +51,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "A favor, são essenciais para a sustentabilidade",
-                afirmacao: "politicas públicas que desencorajam a reciclagem podem ser vistas como prejudiciais ao mesmo ambiente e á sustentabilidade global."
+                afirmacao: [
+                    "politicas públicas que desencorajam a reciclagem podem ser vistas como prejudiciais ao mesmo ambiente e á sustentabilidade global.",
+
+                ]
             },
             {
                 texto: "Contra, acredito que não são eficazes",
-                afirmacao: "Sou contra, pois acredito que essas políticas públicas não são eficazes. Muitas vezes, a implementação é inadequada, faltando infraestrutura e educação adequada para a população."
+                afirmacao: [
+                    "Sou contra, pois acredito que essas políticas públicas não são eficazes. Muitas vezes, a implementação é inadequada, faltando infraestrutura e educação adequada para a população.",
+
+                ]
             }
         ]
     },
@@ -49,11 +70,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Evito ao máximo e uso alternativas reutilizáveis.",
-                afirmacao: "Evito, ao máximo, o uso de produtos descartáveis e busco sempre utilizar alternativas reutilizáveis."
+                afirmacao: [
+                    "Evito, ao máximo, o uso de produtos descartáveis e busco sempre utilizar alternativas reutilizáveis.",
+
+                ]
             },
             {
                 texto: "Uso sem preucupação com o impacto ambiental.",
-                afirmacao: "E errado ter essa atitude em relação a produtos descartáveis porque eles contribuem significativamente para a poluição."
+                afirmacao: [
+                    "E errado ter essa atitude em relação a produtos descartáveis porque eles contribuem significativamente para a poluição.",
+
+                ]
             }
         ]
     }, 
@@ -62,11 +89,18 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, sempre pesquiso e acompanho o destino dos meus resíduos.",
-                afirmacao: "Você está no caminho certo, se informar sobre o destino dos seus resíduos é muito importante."
+                afirmacao: [
+                    "Você está no caminho certo, se informar sobre o destino dos seus resíduos é muito importante.",
+
+                ]
+
             },
             {
                 texto: "Nunca me informo, não vejo a necessidade.",
-                afirmacao: "É errado, pois a falta de conhecimento pode contribuir para o descarte inadequado de lixo, agravando problemas como a poluição ambiental. "
+                afirmacao: [
+                    "É errado, pois a falta de conhecimento pode contribuir para o descarte inadequado de lixo, agravando problemas como a poluição ambiental. ",
+
+                ]
             }
         ]
     },
@@ -98,7 +132,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
