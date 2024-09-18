da37963dc84a2c618a3d20f31979b588866e7396
aninhagrp <103213843+aninhagrp@users.noreply.github.com>
2024-09-18 09:16:58 -0300

    atualizando o js

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Ana luiza", "Miguel", "Grazielly", "Kaike",];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
